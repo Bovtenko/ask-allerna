424cc994a26289b016dc95b6bc2d65e87fb6e008
Bovtenko <obovtenko@gmail.com>
2025-08-14 20:14:19 +0000

    Add back full Ask Allerna component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
+import AskAllerna from '../components/AskAllerna'
+
 export default function Home() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Hello World - Ask Allerna</h1>
-      <p>This is a test page</p>
-    </div>
-  )
+  return <AskAllerna />
 }
